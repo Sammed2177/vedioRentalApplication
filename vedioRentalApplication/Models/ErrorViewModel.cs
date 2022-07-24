65f6c79abd66f2697ce034e3861625ec2d240a0e
Sammed2177 <sammedbiraje@gmail.com>
2022-07-25 00:00:15 +0530

    Add project files.

---
+++ vedioRentalApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vedioRentalApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
